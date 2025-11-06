8a70cbf934c244d90c4a6e455676defb01650587
tomux <thomasagomez77@gmail.com>
2025-11-05 21:49:35 -0600

    update shell script

--- setup.sh
+++ setup.sh
@@ -9,10 +9,16 @@ sudo pacman -Syu --noconfirm
 sudo pacman -S --noconfirm neovim git curl htop tmux yay
 yay -S --noconfirm ghostty
 
-
 # Copy dotfiles
 cp ./dotfiles/.bashrc ~/.bashrc
 cp ./dotfiles/.vimrc ~/.vimrc
+
+# Set environment variables
+echo 'export EDITOR=nvim' >> ~/.bashrc
+
+# Enable services
+sudo systemctl enable bluetooth.service
+sudo systemctl start bluetooth.service
 
 # Create workspace
 mkdir -p ~/Projects ~/Games ~/Screenshots
